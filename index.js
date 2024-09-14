48d7f9b64c4e5ef31bf5a9fcaf8a8614ee2fc413
Abraão Cezar <131885140+cezarbtw@users.noreply.github.com>
2024-09-14 16:03:26 -0300

    fundamentos 002

---
+++ index.js
@@ -0,0 +1,14 @@
+// arrays, objetos
+let meta = {
+    value: "ler um livro todo mes",
+    checked: true,
+
+}
+let metas = [
+    meta,
+    {
+        value: "caminhar 20 minutos todos os dias",
+        checked : false
+    }
+]
+console.log (metas[1].value);
