f167949793d94fb5bbb9c5820c72ac457c697a24
Abraão Cezar <131885140+cezarbtw@users.noreply.github.com>
2024-09-14 19:03:29 -0300

    adiciona inquirer

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
-// arrays, objetos
-let meta = {
-    value: "ler um livro todo mes",
-    checked: true,
+const start = () => {
+    
+    while(true){
+        let opcao = "sair"
+        switch(opcao){
+            case "cadastrar":
+                console.log("vamos cadastrar")
+                break
+            case "listar":
+                console.log("vamos listar")
+                break
+            case "sair":
+                return
 
-}
-let metas = [
-    meta,
-    {
-        value: "caminhar 20 minutos todos os dias",
-        checked : false
+        }
     }
-]
-console.log (metas[1].value);
+}
+start()
