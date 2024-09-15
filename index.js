caccfe43a265c7cbcb3622d4d8f8d1eb3fc95f4c
Abraão Cezar <131885140+cezarbtw@users.noreply.github.com>
2024-09-15 19:46:44 -0300

    commit

--- index.js
+++ index.js
@@ -1,15 +1,41 @@
-const start = () => {
+const { select, input} = require ('@inquirer/prompts')
+
+let meta = {
+    value : "Tomar 4L De agua por dia",
+    checked: false,
+}
+let metas = [ meta ]
+
+const cadastrarMeta = async () => {
+    const meta = await input ({ message: "Digite a meta:"})
+
+    if (meta.length == 0 ){
+        console.log ("A meta nao pode ser vazia.")
+        return
+    }
+    
+    metas.push(
+        { value: meta, checked: false}
+    )
+}
+
+const start = async () => {
     
     while(true){
-        let opcao = "sair"
+        
+        const opcao = await select ({ 
+        })
+
         switch(opcao){
             case "cadastrar":
-                console.log("vamos cadastrar")
+                await cadastrarMeta()
+                console.log(metas)
                 break
             case "listar":
                 console.log("vamos listar")
                 break
             case "sair":
+                console.log("Até a próxima!")
                 return
 
         }
